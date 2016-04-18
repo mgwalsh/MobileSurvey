3b134f3422aabbdf9b6903210041718724c74dad
mgwalsh <markusgwalsh@gmail.com>
2016-04-18 09:40:47 +0300

    documentation update

--- TZ_maize_alluvial.R
+++ TZ_maize_alluvial.R
@@ -17,10 +17,10 @@ setwd("./TZ_data")
 download("https://www.dropbox.com/s/5io90qtmwdzewmi/TZ_crop_scout.csv?dl=0", "TZ_crop_scout.csv", mode="wb")
 crps <- read.table("TZ_crop_scout.csv", header=T, sep=",")
 
-maize <- as.data.frame(table(crps$Maize, crps$Sorghum, crps$Legume, crps$Root, crps$Other, crps$Livestock))
-colnames(maize) <- c("Maize","Sorghum","Legume","Root","Other","Livestock","Freq")
+maize <- as.data.frame(table(crps$Maize, crps$Legume, crps$Root, crps$Other, crps$Livestock))
+colnames(maize) <- c("Maize","Legume","Root","Other","Livestock","Freq")
 
 # <alluvial> diagram ------------------------------------------------------
-alluvial(maize[,1:6], freq=maize$Freq, border=NA,
-         hide = maize$Freq < quantile(maize$Freq, 0.75),
+alluvial(maize[,1:5], freq=maize$Freq, border=NA,
+         hide = maize$Freq < quantile(maize$Freq, 0.50),
          col=ifelse(maize$Maize == "Yes", "red", "gray"))
