2f68a3cbe5a9cc23a1f830d2433ac2502cfec3e2
mgwalsh <markusgwalsh@gmail.com>
2017-07-13 15:52:19 +0200

    data setup

---
+++ TZ_crop_prod.R
@@ -0,0 +1,37 @@
+# Tanzania cropping system productivity indicators
+# M. Walsh, June 2017
+
+require(downloader)
+require(rgdal)
+require(raster)
+
+# Data setup --------------------------------------------------------------
+# Create a data folder in  your current working directory
+dir.create("TZ_npp", showWarnings=F)
+setwd("./TZ_npp")
+
+# Download
+# MobileSurvey data
+download("https://www.dropbox.com/s/vz6cxhsdrkznmkm/TZ_maize_system.csv.zip?raw=1", "TZ_maize_system.csv.zip", mode="wb")
+unzip("TZ_maize_system.csv.zip", overwrite=T)
+crps <- read.table("TZ_maize_system.csv", header=T, sep=",")
+
+# Productivity grids (NPP parameters & RUE)
+download("https://www.dropbox.com/s/hrnkbpkabt3a5kj/TZ_npp.zip?raw=1", "TZ_npp.zip", mode="wb")
+unzip("TZ_npp.zip", overwrite=T)
+glist <- list.files(pattern="tif", full.names=T)
+grids <- stack(glist)
+
+# Overlay with gridded covariates -----------------------------------------
+# Project survey coords to grid CRS
+crps.proj <- as.data.frame(project(cbind(crps$lon, crps$lat), "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs"))
+colnames(crps.proj) <- c("x","y") ## laea coordinates
+crps <- cbind(crps, crps.proj)
+coordinates(crps) <- ~x+y
+projection(crps) <- projection(grids)
+
+# Extract gridded variables at MobileSurvey locations
+crpsgrid <- extract(grids, crps)
+crps <- as.data.frame(crps)
+crps <- cbind.data.frame(crps, crpsgrid)
+crps <- unique(na.omit(crps)) ## includes only unique & complete records
