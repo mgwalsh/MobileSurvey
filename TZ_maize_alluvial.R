ddde56bb8375c36bfd6bdc2386b03d296cefc4cd
mgwalsh <markusgwalsh@gmail.com>
2017-07-13 14:20:43 +0200

    data update

--- TZ_maize_alluvial.R
+++ TZ_maize_alluvial.R
@@ -2,8 +2,8 @@
 # M. Walsh, February 2016
 
 # install alluvial package
-require(devtools)
-install_github("mbojan/alluvial")
+# require(devtools)
+# install_github("mbojan/alluvial")
 
 require(downloader)
 require(alluvial)
@@ -14,13 +14,14 @@ dir.create("TZ_data", showWarnings=F)
 setwd("./TZ_data")
 
 # Download
-download("https://www.dropbox.com/s/5io90qtmwdzewmi/TZ_crop_scout.csv?raw=1", "TZ_crop_scout.csv", mode="wb")
-crps <- read.table("TZ_crop_scout.csv", header=T, sep=",")
+download("https://www.dropbox.com/s/vz6cxhsdrkznmkm/TZ_maize_system.csv.zip?raw=1", "TZ_maize_system.csv.zip", mode="wb")
+unzip("TZ_maize_system.csv.zip", overwrite=T)
+crps <- read.table("TZ_maize_system.csv", header=T, sep=",")
 
-maize <- as.data.frame(table(crps$Maize, crps$Legume, crps$Root, crps$Other, crps$Livestock))
-colnames(maize) <- c("Maize","Legume","Root","Other","Livestock","Freq")
+maize <- as.data.frame(table(crps$MZP, crps$SGP, crps$LGP, crps$RCP, crps$OCP, crps$LVS))
+colnames(maize) <- c("Maize","Sorghum","Legume","Root","Other","Livestock","Freq")
 
 # <alluvial> diagram ------------------------------------------------------
-alluvial(maize[,1:5], freq=maize$Freq, border=NA,
+alluvial(maize[,1:6], freq=maize$Freq, border=NA,
          hide = maize$Freq < quantile(maize$Freq, 0.50),
-         col=ifelse(maize$Maize == "Yes", "red", "gray"))
+         col=ifelse(maize$Maize == "Y", "red", "gray"))
