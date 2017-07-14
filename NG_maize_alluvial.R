3333ae9c96d4da790e5307370e9ecdc1abd0ddc5
mgwalsh <markusgwalsh@gmail.com>
2017-07-14 23:14:11 +0200

    data update

--- NG_maize_alluvial.R
+++ NG_maize_alluvial.R
@@ -14,13 +14,13 @@ dir.create("NG_data", showWarnings=F)
 setwd("./NG_data")
 
 # Download
-download("https://www.dropbox.com/s/jd3gd4qifoknuqb/OCP_crop_scout.csv?raw=1", "OCP_crop_scout.csv", mode="wb")
+download("https://www.dropbox.com/s/jt913s924li8ruz/NG_crop_scout_0717.csv.zip?raw=1", "OCP_crop_scout.csv", mode="wb")
 crps <- read.table("OCP_crop_scout.csv", header=T, sep=",")
 
-maize <- as.data.frame(table(crps$Maize, crps$Legume, crps$Root, crps$Other, crps$Livestock))
-colnames(maize) <- c("Maize","Legume","Root","Other","Livestock","Freq")
+maize <- as.data.frame(table(crps$MZP, crps$SGP, crps$LGP, crps$RCP, crps$OCP, crps$LVS))
+colnames(maize) <- c("Maize","Sorghum","Legume","Root","Other","Livestock","Freq")
 
 # <alluvial> diagram ------------------------------------------------------
-alluvial(maize[,1:5], freq=maize$Freq, border=NA,
+alluvial(maize[,1:6], freq=maize$Freq, border=NA,
          hide = maize$Freq < quantile(maize$Freq, 0.50),
-         col=ifelse(maize$Maize == "Yes", "red", "gray"))
+         col=ifelse(maize$Maize == "Y", "red", "gray"))
