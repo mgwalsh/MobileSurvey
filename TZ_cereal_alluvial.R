48f2bf4cd24336492e86990dd0456016bbb1ecff
mgwalsh <markusgwalsh@gmail.com>
2017-12-29 14:35:57 +0300

    Alluvial diagram of TZ cereal cropping systems

--- TZ_cereal_alluvial.R
+++ TZ_cereal_alluvial.R
@@ -20,16 +20,22 @@ crps <- read.table("TZ_cereal_system.csv", header=T, sep=",")
 
 # Data setup --------------------------------------------------------------
 crps$CRP <- ifelse(crps$CCP == "Y" | crps$LCP =="Y" | crps$RCP == "Y" | crps$OCP == "Y", "Y", "N")
+
+# cropping systems partion
 crplnd <- as.data.frame(table(crps$CRP, crps$CCP, crps$LCP, crps$RCP, crps$OCP, crps$LVP))
 colnames(crplnd) <- c("Cropland","Cereal","Legume","Root","Other","Livestock","Freq")
+
+# cereal systems partion
 cereal <- as.data.frame(table(crps$CCP, crps$MZP, crps$SGP, crps$RIP, crps$LCP, crps$RCP, crps$OCP, crps$LVP))
 colnames(cereal) <- c("Cereal","Maize","Sorghum","Rice","Legume","Root","Other","Livestock","Freq")
 
 # <alluvial> diagram ------------------------------------------------------
+# cropland systems
 alluvial(crplnd[,1:6], freq=crplnd$Freq, border=NA,
          hide = crplnd$Freq < quantile(crplnd$Freq, 0.90),
          col=ifelse(crplnd$Cropland == "Y", "red", "gray"))
 
+# cereal systems
 alluvial(cereal[,1:8], freq=cereal$Freq, border=NA,
          hide = cereal$Freq < quantile(cereal$Freq, 0.90),
          col=ifelse(cereal$Cereal == "Y", "red", "gray"))
