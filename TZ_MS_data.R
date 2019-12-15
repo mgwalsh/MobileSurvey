ad342dc1184737c666a1bd4451d73e25bc86e63e
mgwalsh <markusgwalsh@gmail.com>
2019-12-15 03:54:25 -0700

    Tanzania MobileSurvey 250m resolution data setup

--- TZ_MS_data.R
+++ TZ_MS_data.R
@@ -20,7 +20,7 @@ dir.create("Results", showWarnings = F)
 # download MobileSurvey data
 download("https://osf.io/phu4b?raw=1", "TZ_msdat_2019.csv.zip", mode="wb")
 unzip("TZ_msdat_2019.csv.zip", overwrite = T)
-msdat <- read.table("TZ_msdat.csv", header=T, sep=",")
+msdat <- read.table("TZ_msdat_2019.csv", header=T, sep=",")
 
 # download GADM-L3 shapefile (courtesy: http://www.gadm.org)
 download("https://www.dropbox.com/s/bhefsc8u120uqwp/TZA_adm3.zip?raw=1", "TZA_adm3.zip", mode = "wb")
@@ -40,18 +40,18 @@ projection(msdat) <- projection(shape)
 gadm <- msdat %over% shape
 msdat <- as.data.frame(msdat)
 msdat <- cbind(gadm[ ,c(5,7,9)], msdat)
-colnames(msos)[1:3] <- c("region","district","ward")
+colnames(msdat)[1:3] <- c("region","district","ward")
 
 # project MobileSurvey coords to grid CRS
-msos.proj <- as.data.frame(project(cbind(msos$lon, msos$lat), "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs"))
-colnames(msos.proj) <- c("x","y")
-msos <- cbind(msos, msos.proj)
-coordinates(msos) <- ~x+y
-projection(msos) <- projection(grids)
+msdat.proj <- as.data.frame(project(cbind(msdat$lon, msdat$lat), "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs"))
+colnames(msdat.proj) <- c("x","y")
+msdat <- cbind(msdat, msdat.proj)
+coordinates(msdat) <- ~x+y
+projection(msdat) <- projection(grids)
 
 # extract gridded variables at MobileSurvey locations
-msosgrid <- extract(grids, msos)
-msdat <- as.data.frame(cbind(msos, msosgrid)) 
+msdatgrid <- extract(grids, msdat)
+msdat <- as.data.frame(cbind(msdat, msdatgrid)) 
 
 # Write output files ------------------------------------------------------
 write.csv(msdat, "./Results/TZ_msdat.csv", row.names = F)
